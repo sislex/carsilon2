29c6d3f9360b043c48f2e64b8da39c8a3c76a416
Alex Rozhnov <alex.rozhnov@capsilon.com>
2019-07-18 18:18:26 +0300

    added destination, add route

---
+++ src/app/pages/destination-form/destination-form.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'destination-form',
+  templateUrl: './destination-form.page.html',
+  styleUrls: ['./destination-form.page.scss'],
+})
+export class DestinationFormPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
